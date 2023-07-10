08e7832c91515561b23a54492933c0ec13990d92
aelshal-1 <a.elshal@icloud.com>
2023-07-10 21:07:11 +0300

    add gitignore file

--- build/CMakeFiles/Builder.dir/DependInfo.cmake
+++ build/CMakeFiles/Builder.dir/DependInfo.cmake
@@ -8,15 +8,15 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "D:/Projects/C++/JsonBuilder/JsonArray.cpp" "CMakeFiles/Builder.dir/JsonArray.cpp.obj" "gcc" "CMakeFiles/Builder.dir/JsonArray.cpp.obj.d"
-  "D:/Projects/C++/JsonBuilder/JsonBuilder.cpp" "CMakeFiles/Builder.dir/JsonBuilder.cpp.obj" "gcc" "CMakeFiles/Builder.dir/JsonBuilder.cpp.obj.d"
-  "D:/Projects/C++/JsonBuilder/JsonElement.cpp" "CMakeFiles/Builder.dir/JsonElement.cpp.obj" "gcc" "CMakeFiles/Builder.dir/JsonElement.cpp.obj.d"
-  "D:/Projects/C++/JsonBuilder/JsonObject.cpp" "CMakeFiles/Builder.dir/JsonObject.cpp.obj" "gcc" "CMakeFiles/Builder.dir/JsonObject.cpp.obj.d"
-  "D:/Projects/C++/JsonBuilder/main.cpp" "CMakeFiles/Builder.dir/main.cpp.obj" "gcc" "CMakeFiles/Builder.dir/main.cpp.obj.d"
+  "D:/Projects/C++/JSON-Builder/JsonArray.cpp" "CMakeFiles/Builder.dir/JsonArray.cpp.obj" "gcc" "CMakeFiles/Builder.dir/JsonArray.cpp.obj.d"
+  "D:/Projects/C++/JSON-Builder/JsonBuilder.cpp" "CMakeFiles/Builder.dir/JsonBuilder.cpp.obj" "gcc" "CMakeFiles/Builder.dir/JsonBuilder.cpp.obj.d"
+  "D:/Projects/C++/JSON-Builder/JsonElement.cpp" "CMakeFiles/Builder.dir/JsonElement.cpp.obj" "gcc" "CMakeFiles/Builder.dir/JsonElement.cpp.obj.d"
+  "D:/Projects/C++/JSON-Builder/JsonObject.cpp" "CMakeFiles/Builder.dir/JsonObject.cpp.obj" "gcc" "CMakeFiles/Builder.dir/JsonObject.cpp.obj.d"
+  "D:/Projects/C++/JSON-Builder/main.cpp" "CMakeFiles/Builder.dir/main.cpp.obj" "gcc" "CMakeFiles/Builder.dir/main.cpp.obj.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/Projects/C++/JsonBuilder
+# Install script for directory: D:/Projects/C++/JSON-Builder
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/JsonBuilder")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Builder")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/MinGW/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/msys64/mingw64/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Projects/C++/JsonBuilder/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Projects/C++/JSON-Builder/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
